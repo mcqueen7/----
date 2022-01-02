7ee2143f396c6168cb5ace50c4d683b51fa4b376
mcqueen <chqueen.tw@gmail.com>
2022-01-03 00:06:15 +0800

    index commit

--- js/index.js
+++ js/index.js
@@ -1,27 +1,48 @@
 $(document).ready(function(){
     $('.nav_link_now').parent().css('background-color',"rgba(192, 37, 231, 0.705)");
 
+    $("#gototop").click(function() {
+      $('html,body').animate({ scrollTop: 0 }, 'ease-in-out');
+      return false;
+    });
+
+  $(function() {
+      $(".flexslider").flexslider({
+      slideshowSpeed: 3700, //展示时间间隔ms
+      animationSpeed: 500, //滚动时间ms
+      touch: true //是否支持触屏滑动
+    });
+  }); 
+  
+  
+  
+  
+    window.onscroll = function() {
+      var scrollTop = $(document).scrollTop();
+      if (scrollTop > 100) {
+           $("#gototop").fadeIn();
+      } else {
+          $("#gototop").fadeOut();
+    };
+  };
+  
+  
+  
+  
+
+  // nav 
+    let menu_click=false;
+    $(".navbar-toggler").click(function(){
+      menu_click=!menu_click;
+      if(menu_click){
+          $(".navbar-icon-bar").attr("menu-click","true");
+      }
+      else{
+          $(".navbar-icon-bar").attr("menu-click","false");
+    }
+  });
+  
+  
+  
 });
 
-$(function() {
-    $(".flexslider").flexslider({
-    slideshowSpeed: 3700, //展示时间间隔ms
-    animationSpeed: 500, //滚动时间ms
-    touch: true //是否支持触屏滑动
-  });
-  $("#gototop").click(function() {
-    $('html,body').animate({ scrollTop: 0 }, 'slow');
-    return false;
-  });
-}); 
-
-
-window.onscroll = function() {
-var scrollTop = $(document).scrollTop();
-if (scrollTop > 20) {
-     $("#gototop").fadeIn();
-} else {
-    $("#gototop").fadeOut();
-};
-
-};
